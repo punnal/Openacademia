65332a42b878d1ddf463bdcfa189de22fe149fc9
AhmadHumayun <ahmadhumayun3.5@gmail.com>
2020-11-21 16:26:24 +0500

    Use Cases Covered: About Us, Filter By Category

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import logo from "./logo.svg";
 import Table from "./Table";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import dbPull from "./api";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const renderLogo = () => {
+const Logo = () => {
   return <img src={logo} className="App-logo" alt="logo" />;
 };
 
+const Heading = () => {
+  return <h1 className="App-header">OpenAcademia</h1>;
+};
+
+const FilterBar = (props) => {
+  return (
+    <ButtonGroup onClick={(e) => props.setFilter(e.target.innerText)}>
+      {categories.map((cat) => (
+        <Button variant="secondary">{cat}</Button>
+      ))}
+    </ButtonGroup>
+  );
+};
+
+const categories = [
+  "All",
+  "Mathematics",
+  "Physics",
+  "Computer Science",
+  "Chemistry",
+  "Biology",
+  "Economics",
+  "Other",
+];
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/aboutus">About Us</Nav.Link>
+        </Nav>
+      </Navbar>
+    </>
+  );
+};
 const App = () => {
   const [success, setSuccess] = useState(false);
+  const [rows, setRows] = useState([]);
+  const [filter, setFilter] = useState("All");
 
   useEffect(() => {
-    console.log("fetching from db...");
-    dbPull("/table/User", (json, _) => {
+    dbPull("/table/Paper", (json, _) => {
+      setRows(json);
       setSuccess(true);
       console.log(json);
     });
@@ -22,10 +68,33 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {renderLogo()}
-        <h1>OpenAcademia</h1>
-        {success ? <Table table /> : null}
+        <Logo />
+        <Heading />
       </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <FilterBar setFilter={setFilter} />
+            {success && (
+              <Table className="App-header" rows={rows} filter={filter} />
+            )}
+          </Route>
+          <Route path="/aboutus">
+            <div className="about-section">
+              <h1>About OpenAcademia</h1>
+              <p>
+                Openacademia is a new system that would encourage authors to
+                archive their research papers. It would not only give
+                researchers full access to the papers online but also allow them
+                to download them and make use of it while being offline. Users
+                can also post a detailed review or respond to some other user’s
+                review.
+              </p>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,32 @@
-const Table = () => {
-  return <h1> YOLO </h1>;
+import "./App.css";
+import BTable from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+const RowCell = (props) => {
+  return <td>{props.attr.toString()}</td>;
+};
+const Row = (props) => {
+  console.log(props.row);
+  return (
+    <tr>
+      {props.row.map((attr) => (
+        <RowCell attr={attr} />
+      ))}
+    </tr>
+  );
+};
+
+const Table = (props) => {
+  return (
+    <BTable striped bordered hover variant="dark">
+      <tbody>
+        {props.rows.map((row, id) =>
+          row.includes(props.filter) || props.filter === "All" ? (
+            <Row key={id} row={row} />
+          ) : null
+        )}
+      </tbody>
+    </BTable>
+  );
 };
 
 export default Table;
