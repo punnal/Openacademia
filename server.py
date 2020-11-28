c47d5c1b54037d8eabd00cee84451a6c752b8261
punnal <21100019@lums.edu.pk>
2020-11-28 23:59:30 +0500

    Add: query api in server

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ app = Flask(__name__, static_folder="../static/dist",
 DB = sqlite3.connect("openacademia.db")
 CURSOR = DB.cursor()
 
+def getID():
+    return 1234567
 
 @app.route("/")
 def index():
@@ -21,6 +23,36 @@ def getTable(table):
     print(resp)
     return jsonify(resp)
 
+
+@app.route('/query', methods=['GET', 'POST'])
+def parse_request():
+    data = request.data
+    print("data: ", data)
+    resp = CURSOR.execute(data.query).fetchall()
+    print("response: ", resp)
+    return jsonify(resp)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def parse_request():
+    data = request.data
+    print("data: ", data)
+    
+    result = CURSOR.execute(f"
+        SELECT email
+                            "
+    )
+
+    userID = getID()
+
+    resp = CURSOR.execute(f"
+        INSERT INTO User [(UserID, Email, Name, IsAuthor, Password)]
+        VALUES ({userID}, {data.email}, {data.name}, 0, {data.password});
+        ").fetchall()
+
+    resp = {"Success": True}
+    print("response: ", resp)
+    return jsonify(resp)
+   
 
 if __name__ == "__main__":
     app.run()
