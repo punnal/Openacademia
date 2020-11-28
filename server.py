296348420e12a04fdbec423b7cd35952f14e90a6
punnal <21100019@lums.edu.pk>
2020-11-29 00:56:45 +0500

    Add: sign in and sign up api

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sqlite3
 from contextlib import closing
 from flask import Flask, render_template, jsonify, request
+import secrets
 
 app = Flask(__name__, static_folder="../static/dist",
             template_folder="../static")
@@ -10,6 +11,9 @@ CURSOR = DB.cursor()
 
 def getID():
     return 1234567
+
+def getSessionID():
+    return secrets.token_urlsafe(64)
 
 @app.route("/")
 def index():
@@ -36,18 +40,38 @@ def exec_query():
 def signup():
     data = request.data
     print("data: ", data)
-    
    
-    resp = CURSOR.execute(f"SELECT {data.email} FROM User WHERE email={data.email}").fetchall()
+    result = CURSOR.execute(f"SELECT {data.email} FROM User WHERE email={data.email}").fetchall()
 
     userID = getID()
 
-    resp = CURSOR.execute(f"INSERT INTO User [(UserID, Email, Name, IsAuthor, Password)] VALUES ({userID}, {data.email}, {data.name}, 0, {data.password});").fetchall()
+    if(not result):
+        resp = CURSOR.execute(f"INSERT INTO User [(UserID, Email, Name, IsAuthor, Password)] VALUES ({userID}, {data.email}, {data.name}, 0, {data.password});").fetchall()
 
-    resp = {"Success": True}
-    print("response: ", resp)
-    return jsonify(resp)
+        sessionID = getSessionID()
+        resp = CURSOR.execute(f"INSERT INTO Sessions VALUES ({sessionID}, {userID});").fetchall()
+
+        resp = {"Success": True, "SessionID": sessionID, "UserID": userID}
+        print("response: ", resp)
+        return jsonify(resp)
+    resp = {"Success": False, "Msg": "User already exists"}
+    return resp
+ 
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    data = request.data
+    print("data: ", data)
    
+    result = CURSOR.execute(f"SELECT * FROM User WHERE email={data.email} AND password={data.password}").fetchall()
+
+    if(result):
+        resp = result
+        print("response: ", resp)
+        return jsonify(resp)
+    else:
+        resp = {"Success": False, "Msg": "Wrong password or account does not exists"}
+        return resp
+  
 
 if __name__ == "__main__":
     app.run()
