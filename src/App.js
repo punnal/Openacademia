b64d107f03961839dc85fd2f56c54ad5c9785b9a
AhmadHumayun <ahmadhumayun3.5@gmail.com>
2020-11-28 23:36:01 +0500

    Changed query method

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Table from "./Table";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
 import Nav from "react-bootstrap/Nav";
-import dbPull from "./api";
+import dbPull from "./dbPull";
+import dbPush from "./dbPush";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
@@ -39,17 +42,80 @@ const categories = [
   "Other",
 ];
 
-const NavBar = () => {
+const SignUp = (props) => {
+  return (
+    <Form inline>
+      <FormControl type="text" placeholder="Email" className="mr-sm-2" />
+      <FormControl type="password" placeholder="Password" className="mr-sm-2" />
+      <Button variant="outline-info" onClick={() => dbPush("/signup")}>
+        Sign Up
+      </Button>
+      <Button onClick={() => props.set.signUp(false)} variant="dark">
+        Back
+      </Button>
+    </Form>
+  );
+};
+const SignIn = (props) => {
+  return (
+    <Form inline>
+      <FormControl type="text" placeholder="Email" className="mr-sm-2" />
+      <FormControl type="password" placeholder="Password" className="mr-sm-2" />
+      <Button variant="outline-info">Sign Up</Button>
+      <Button onClick={() => props.set.signIn(false)} variant="dark">
+        Back
+      </Button>
+    </Form>
+  );
+};
+const SignInSignUp = (props) => {
+  return (
+    <>
+      <Nav.Link onClick={() => props.set.signIn(true)} className="mr-sm-2">
+        Sign In
+      </Nav.Link>
+      <Nav.Link onClick={() => props.set.signUp(true)} className="mr-sm-2">
+        Sign Up
+      </Nav.Link>
+    </>
+  );
+};
+
+const NavBar = (props) => {
+  const [signUp, setSignUp] = useState(false);
+  const [signIn, setSignIn] = useState(false);
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Brand href="/"></Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/aboutus">About Us</Nav.Link>
         </Nav>
+        <Nav>
+          {props.loggedIn ? (
+            <Navbar.Text>Signed in as: {props.username}</Navbar.Text>
+          ) : signUp ? (
+            <SignUp set={{ signUp: setSignUp }} />
+          ) : signIn ? (
+            <SignIn set={{ signIn: setSignIn }} />
+          ) : (
+            <SignInSignUp set={{ signUp: setSignUp, signIn: setSignIn }} />
+          )}
+        </Nav>
       </Navbar>
     </>
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <Nav className="p-2 d-flex justify-content-center">
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Nav>
   );
 };
 const App = () => {
@@ -58,7 +124,7 @@ const App = () => {
   const [filter, setFilter] = useState("All");
 
   useEffect(() => {
-    dbPull("/table/Paper", (json, _) => {
+    dbPush("/query", { query: "select * from Paper" }, (json, _) => {
       setRows(json);
       setSuccess(true);
       console.log(json);
@@ -73,6 +139,7 @@ const App = () => {
       </header>
       <Router>
         <NavBar />
+        <SearchBar />
         <Switch>
           <Route exact path="/">
             <FilterBar setFilter={setFilter} />
